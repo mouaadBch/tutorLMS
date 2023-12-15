8fadb45ba55c7bcea878cff8d25a2d41916b0d2c
MOUAAD BELLOUCHE <mouadbellouche@gmail.com>
2023-12-15 17:54:12 +0100

    fix error in somme users enrolement

--- application/models/Super_admin_model.php
+++ application/models/Super_admin_model.php
@@ -23,26 +23,28 @@ class Super_admin_model extends CI_Model
     {
         return $this->db->where('role_id', 2)->where('is_instructor', 1)->get('users')->num_rows();
     }
+
     #get nombre totale user enroled in my site
     function get_nbr_user_enroled()
     {
         $enroled_ebook = $this->db->select('user_id')->group_by('user_id')->get('ebook_payment');
         $nbr_enroled_ebook = $enroled_ebook->num_rows();
         $enroled_ebook_users = $enroled_ebook->result_array();
-        $enroled_ebook_user_ids = array_column($enroled_ebook_users, 'user_id');
-        if (!empty($enroled_ebook_user_ids)) {
-            $nbr_enroled_course = $this->db
-                ->select('user_id')
-                ->where_not_in('user_id', $enroled_ebook_user_ids)
-                ->where('expiry_date >=', time() + 86400)
-                ->or_where('expiry_date IS NULL')
-                ->group_by('user_id')
-                ->get('enrol')
-                ->num_rows();
-        } else {
-            // If $enroled_ebook_user_ids is empty, handle it accordingly
-            $nbr_enroled_course = 0;
+        $enroled_ebook_user_ids = [];
+
+        foreach ($enroled_ebook_users as $enroled_ebook_user) {
+            $enroled_ebook_user_ids[] = $enroled_ebook_user['user_id'];
         }
+
+        $this->db->select('user_id');
+        if (!empty($enroled_ebook_user_ids)) {
+            $this->db->where_not_in('user_id', $enroled_ebook_user_ids);
+        }
+        $this->db->where('expiry_date >=', time() + 86400);
+        $this->db->or_where('expiry_date IS NULL');
+        $this->db->group_by('user_id');
+
+        $nbr_enroled_course = $this->db->get('enrol')->num_rows();
 
         return $nbr_enroled_ebook +  $nbr_enroled_course;
     }
@@ -51,6 +53,6 @@ class Super_admin_model extends CI_Model
     {
         $nbr_courses = $this->db->get('course')->num_rows();
         $nbr_ebooks = $this->db->get('ebook')->num_rows();
-        return $nbr_courses+$nbr_ebooks;
+        return $nbr_courses + $nbr_ebooks;
     }
 }
